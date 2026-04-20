1b647faa5f2569a8a2307e5929ba1ed14f8c0946
NBA2K1 <78034913+NBA2K1@users.noreply.github.com>
2026-04-20 23:16:47 +0200

    Fix Exception
    Fix Exception introduced in commit b7102776fe533d5ebd222d9107ee858a1efbd7a8 by not removing the outer writeTxn

--- lib/modules/widgets/manga_image_card_widget.dart
+++ lib/modules/widgets/manga_image_card_widget.dart
@@ -353,28 +353,26 @@ Future<void> pushToMangaReaderDetail({
   final exists =
       settings!.sortChapterList?.any((e) => e.mangaId == mangaId) ?? false;
   if (!exists) {
-    await isar.writeTxn(() async {
-      settings
-        ..sortChapterList = [
-          ...(settings.sortChapterList ?? []),
-          SortChapter()..mangaId = mangaId,
-        ]
-        ..chapterFilterBookmarkedList = [
-          ...(settings.chapterFilterBookmarkedList ?? []),
-          ChapterFilterBookmarked()..mangaId = mangaId,
-        ]
-        ..chapterFilterDownloadedList = [
-          ...(settings.chapterFilterDownloadedList ?? []),
-          ChapterFilterDownloaded()..mangaId = mangaId,
-        ]
-        ..chapterFilterUnreadList = [
-          ...(settings.chapterFilterUnreadList ?? []),
-          ChapterFilterUnread()..mangaId = mangaId,
-        ]
-        ..updatedAt = DateTime.now().millisecondsSinceEpoch;
+    settings
+      ..sortChapterList = [
+        ...(settings.sortChapterList ?? []),
+        SortChapter()..mangaId = mangaId,
+      ]
+      ..chapterFilterBookmarkedList = [
+        ...(settings.chapterFilterBookmarkedList ?? []),
+        ChapterFilterBookmarked()..mangaId = mangaId,
+      ]
+      ..chapterFilterDownloadedList = [
+        ...(settings.chapterFilterDownloadedList ?? []),
+        ChapterFilterDownloaded()..mangaId = mangaId,
+      ]
+      ..chapterFilterUnreadList = [
+        ...(settings.chapterFilterUnreadList ?? []),
+        ChapterFilterUnread()..mangaId = mangaId,
+      ]
+      ..updatedAt = DateTime.now().millisecondsSinceEpoch;
 
-      await isar.settings.putAndSave(settings);
-    });
+    await isar.settings.putAndSave(settings);
   }
   if (!addToFavourite) {
     if (context.mounted) {
